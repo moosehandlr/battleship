4e1377d20003564cf1c17647d20855bd079fa97c
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-05-12 14:36:49 -0600

    Test ship exist

---
+++ test/ship_test.rb
@@ -0,0 +1,46 @@
+
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/ship"
+
+class ShipTest < Minitest::Test
+  def test_it_exist
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_instance_of Ship, cruiser
+  end
+end
+
+
+# pry(main)> cruiser = Ship.new("Cruiser", 3)
+# #=> #<Ship:0x00007feb05112d10...>
+#
+# pry(main)> cruiser.name
+# #=> "Cruiser"
+#
+# pry(main)> cruiser.length
+# #=> 3
+#
+# pry(main)> cruiser.health
+# #=> 3
+#
+# pry(main)> cruiser.sunk?
+# #=> false
+#
+# pry(main)> cruiser.hit
+#
+# pry(main)> cruiser.health
+# #=> 2
+#
+# pry(main)> cruiser.hit
+#
+# pry(main)> cruiser.health
+# #=> 1
+#
+# pry(main)> cruiser.sunk?
+# #=> false
+#
+# pry(main)> cruiser.hit
+#
+# pry(main)> cruiser.sunk?
+# #=> true
