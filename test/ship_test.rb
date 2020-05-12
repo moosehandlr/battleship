5148969458be928de53327c73b80f0365abb480f
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-05-12 14:56:22 -0600

    test the ship has health

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -16,6 +16,13 @@ class ShipTest < Minitest::Test
     assert_equal "Cruiser", cruiser.name
     assert_equal 3  , cruiser.length
   end
+
+  def test_it_has_health
+    skip
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal 3  , cruiser.health
+  end
 end
 
 
