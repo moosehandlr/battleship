a3e37d11c3aa64ae993e222eb773da4f24c3178c
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-05-12 14:45:01 -0600

    Test for name and length

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -9,6 +9,13 @@ class ShipTest < Minitest::Test
 
     assert_instance_of Ship, cruiser
   end
+
+  def test_it_has_name_and_length
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal "Cruiser", cruiser.name
+    assert_equal 3  , cruiser.length
+  end
 end
 
 
