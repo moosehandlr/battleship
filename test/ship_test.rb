fb1ed643089eca1994508e2582b82072556cf424
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-05-12 23:54:00 -0600

    remove comments

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -55,37 +55,3 @@ class ShipTest < Minitest::Test
     assert_equal true, cruiser.sunk?
   end
 end
-
-
-# pry(main)> cruiser = Ship.new("Cruiser", 3)
-# #=> #<Ship:0x00007feb05112d10...>
-#
-# pry(main)> cruiser.name
-# #=> "Cruiser"
-#
-# pry(main)> cruiser.length
-# #=> 3
-#
-# pry(main)> cruiser.health
-# #=> 3
-#
-# pry(main)> cruiser.sunk?
-# #=> false
-#
-# pry(main)> cruiser.hit
-#
-# pry(main)> cruiser.health
-# #=> 2
-#
-# pry(main)> cruiser.hit
-#
-# pry(main)> cruiser.health
-# #=> 1
-#
-# pry(main)> cruiser.sunk?
-# #=> false
-#
-# pry(main)> cruiser.hit
-#
-# pry(main)> cruiser.sunk?
-# #=> true
