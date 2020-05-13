64f6ebc115c8281cbd31461a0b342bfa5d66cd5d
Alex Desjardins <ardesjardins91@gmail.com>
2020-05-13 15:26:25 -0600

    Add render method test

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -58,10 +58,60 @@ class CellTest < Minitest::Test
     cruiser = Ship.new("Cruiser", 3)
 
     cell.place_ship(cruiser)
-    
+
     assert_equal false, cell.fired_upon?
     cell.fire_upon
     assert_equal true, cell.fired_upon?
     assert_equal 2, cell.ship.health
   end
+
+  def test_for_render
+    cell_1 = Cell.new("B4")
+
+    assert_equal ".", cell_1.render
+  end
 end
+
+# pry(main)> cell_1 = Cell.new("B4")
+# # => #<Cell:0x00007f84f11df920...>
+#
+# pry(main)> cell_1.render
+# # => "."
+#
+# pry(main)> cell_1.fire_upon
+#
+# pry(main)> cell_1.render
+# # => "M"
+#
+# pry(main)> cell_2 = Cell.new("C3")
+# # => #<Cell:0x00007f84f0b29d10...>
+#
+# pry(main)> cruiser = Ship.new("Cruiser", 3)
+# # => #<Ship:0x00007f84f0ad4fb8...>
+#
+# pry(main)> cell_2.place_ship(cruiser)
+#
+# pry(main)> cell_2.render
+# # => "."
+#
+# # Indicate that we want to show a ship with the optional argument
+# pry(main)> cell_2.render(true)
+# # => "S"
+#
+# pry(main)> cell_2.fire_upon
+#
+# pry(main)> cell_2.render
+# # => "H"
+#
+# pry(main)> cruiser.sunk?
+# # => false
+#
+# pry(main)> cruiser.hit
+#
+# pry(main)> cruiser.hit
+#
+# pry(main)> cruiser.sunk?
+# # => true
+#
+# pry(main)> cell_2.render
+# # => "X"
