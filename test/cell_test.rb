f64f55a8d6be74a724c084fe820c28da20ad4583
Alex Desjardins <ardesjardins91@gmail.com>
2020-05-13 12:03:27 -0600

    Test for method place ship

---
+++ test/cell_test.rb
@@ -0,0 +1,45 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+
+class CellTest < Minitest::Test
+
+  def test_it_exists
+    cell = Cell.new("B4")
+
+    assert_instance_of Cell, cell
+  end
+
+
+  def test_cell_coordinate
+    cell = Cell.new("B4")
+
+    assert_equal "B4", cell.coordinate
+  end
+
+
+  def test_if_cell_has_a_ship
+    cell = Cell.new("B4")
+
+    assert_nil nil, cell.ship
+  end
+
+
+  def test_if_cell_is_empty
+    cell = Cell.new("B4")
+
+    assert_equal true, cell.empty?
+  end
+
+  def test_for_ship_placement
+    cruiser = Ship.new("Cruiser", 3)
+    cell = Cell.new("B4")
+
+    cell.place_ship(cruiser)
+
+    assert_instance_of ship, cell.ship
+  end
+
+
+end
