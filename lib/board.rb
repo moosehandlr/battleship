8f574cbc142c77247a4d844f3a73e6b2170613ac
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-05-14 15:11:58 -0600

    create board cell

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,15 @@
+require "./lib/cell"
 
 class Board
+  def cells
+    letter_arr = ("A".."D").to_a.map{|letter| letter*4}.join.split("")
+    number_arr = (1..4).to_a*4
+    mix = letter_arr.zip(number_arr)
+    board_coordinates = mix.map{|arr| arr.join}
+    cell_values = board_coordinates.map{|coord| Cell.new(coord)}
+    # zipp values
+    zipped = board_coordinates.zip(cell_values)
 
+    Hash[zipped]
+  end
 end
