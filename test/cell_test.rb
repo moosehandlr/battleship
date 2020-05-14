fef12b9cf32dc8d23a6e588f019ee71a949473a7
Alex Desjardins <ardesjardins91@gmail.com>
2020-05-13 19:58:11 -0600

    Test cell can render H when hit.

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -112,6 +112,26 @@ class CellTest < Minitest::Test
     assert_equal ".", cell_2.render
     assert_equal "S", cell_2.render(true)
   end
+
+  def test_it_renders_H_when_hit
+    # skip
+    cell_1 = Cell.new("B4")
+
+    assert_equal ".", cell_1.render
+    cell_1.fire_upon
+    assert_equal "M", cell_1.render
+
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+
+    cell_2.place_ship(cruiser)
+
+    assert_equal ".", cell_2.render
+    assert_equal "S", cell_2.render(true)
+
+    cell_2.fire_upon
+    assert_equal "H", cell_2.render
+  end
 end
 
 
