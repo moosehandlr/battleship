61d69b6157ae309f0f7af27ba14f0b86f410c19e
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-05-14 15:45:05 -0600

    Test if valid coordinates were entered

--- test/board_test.rb
+++ test/board_test.rb
@@ -17,32 +17,14 @@ class BoardTest < Minitest::Test
     assert_equal "A1", board.cells["A1"].coordinate
   end
 
+  def test_board_has_valid_coodinates
+    board = Board.new
+
+    assert_equal true , board.valid_coordinate?("A1")
+    assert_equal true , board.valid_coordinate?("D4")
+    assert_equal false , board.valid_coordinate?("A5")
+    assert_equal false , board.valid_coordinate?("E1")
+    assert_equal false , board.valid_coordinate?("A22")
+  end
+
 end
-
-
-# pry(main)> require './lib/board'
-# # => true
-#
-# pry(main)> board = Board.new
-# # => #<Board:0x00007ff0728c8010...>
-#
-# pry(main)> board.cells
-# # =>
-# {
-#  "A1" => #<Cell:0x00007ff0728a3f58...>,
-#  "A2" => #<Cell:0x00007ff0728a3ee0...>,
-#  "A3" => #<Cell:0x00007ff0728a3e68...>,
-#  "A4" => #<Cell:0x00007ff0728a3df0...>,
-#  "B1" => #<Cell:0x00007ff0728a3d78...>,
-#  "B2" => #<Cell:0x00007ff0728a3d00...>,
-#  "B3" => #<Cell:0x00007ff0728a3c88...>,
-#  "B4" => #<Cell:0x00007ff0728a3c10...>,
-#  "C1" => #<Cell:0x00007ff0728a3b98...>,
-#  "C2" => #<Cell:0x00007ff0728a3b20...>,
-#  "C3" => #<Cell:0x00007ff0728a3aa8...>,
-#  "C4" => #<Cell:0x00007ff0728a3a30...>,
-#  "D1" => #<Cell:0x00007ff0728a39b8...>,
-#  "D2" => #<Cell:0x00007ff0728a3940...>,
-#  "D3" => #<Cell:0x00007ff0728a38c8...>,
-#  "D4" => #<Cell:0x00007ff0728a3850...>
-# }
