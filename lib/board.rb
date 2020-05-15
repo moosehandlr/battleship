41f08c0c004d5d240e84435aa8ed1bb3f066efe9
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-05-14 18:10:15 -0600

    Add method to generate the y_coordinates

--- lib/board.rb
+++ lib/board.rb
@@ -3,8 +3,8 @@ require "./lib/cell"
 class Board
   def cells
     # letter_arr = ("A".."D").to_a.map{|letter| letter*4}.join.split("")
-    number_arr = (1..4).to_a*4
-    mix = self.x_coordinates.zip(number_arr)
+    # number_arr = (1..4).to_a*4
+    mix = self.x_coordinates.zip(self.y_coordinates)
     board_coordinates = mix.map{|arr| arr.join}
     cell_values = board_coordinates.map{|coord| Cell.new(coord)}
     # zipp values
@@ -15,6 +15,10 @@ class Board
 
   def x_coordinates
     ("A".."D").to_a.map{|letter| letter*4}.join.split("")
+  end
+
+  def y_coordinates
+    (1..4).to_a*4
   end
 
   def valid_coordinate?(coordinate)
