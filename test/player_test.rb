0b004000a21acf0d42a32d06d2b96c38784665f9
Alex Desjardins <ardesjardins91@gmail.com>
2020-05-18 15:02:18 -0600

    Test players has empty board.

--- test/player_test.rb
+++ test/player_test.rb
@@ -7,6 +7,7 @@ require "./lib/board"
 class PlayerTest < Minitest::Test
 
   def test_player_types
+    skip
     player1 = Player.new("human")
     player2 = Player.new("computer")
 
@@ -15,6 +16,14 @@ class PlayerTest < Minitest::Test
   end
 
   def test_players_have_an_empty_board
-    
+    board1 = Board.new
+    board2 = Board.new
+
+    player1 = Player.new("human", board1)
+    player2 = Player.new("computer", board2)
+
+    # require "pry"; binding.pry
+
+    assert_equal board1, player1.board
   end
 end
