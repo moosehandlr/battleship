896315eacd8911bda8e1af00e019dd024470c70b
Alex Desjardins <ardesjardins91@gmail.com>
2020-05-19 16:04:47 -0600

    Update player class and test

--- lib/player.rb
+++ lib/player.rb
@@ -1,14 +1,16 @@
 class Player
   attr_reader :player_type,
-              :board
+              :board,
+              :ships
 
-  def initialize (player_type, board)
+  def initialize (player_type, board, ships)
     @player_type = player_type
     @board = board
+    @ships = ships
 
   end
 
-  def has_lost?
-
+  def add_ship(new_ship)
+    @ships << ship
   end
 end
--- test/player_test.rb
+++ test/player_test.rb
@@ -3,27 +3,66 @@ require "minitest/autorun"
 require "minitest/pride"
 require "./lib/player"
 require "./lib/board"
+require "./lib/ship"
 
 class PlayerTest < Minitest::Test
 
   def test_player_types
-    skip
-    player1 = Player.new("human")
-    player2 = Player.new("computer")
+    cruiser= Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    ships = [cruiser, submarine]
+
+    board1 = Board.new
+    board2 = Board.new
+
+    player1 = Player.new("human", board1, ships)
+    player2 = Player.new("computer", board2, ships)
 
     assert_equal "human", player1.player_type
     assert_equal "computer", player2.player_type
   end
 
-  def test_players_have_an_empty_board
+  def test_players_has_two_ships
+    cruiser= Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    ships = [cruiser, submarine]
+
     board1 = Board.new
     board2 = Board.new
 
-    player1 = Player.new("human", board1)
-    player2 = Player.new("computer", board2)
+    player1 = Player.new("human", board1, ships)
+    player2 = Player.new("computer", board2, ships)
 
-    # require "pry"; binding.pry
+    assert_equal [cruiser, submarine], player1.ships
+  end
+
+  def test_players_have_an_empty_board
+    cruiser= Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    ships = [cruiser, submarine]
+
+    board1 = Board.new
+    board2 = Board.new
+
+    player1 = Player.new("human", board1, ships)
+    player2 = Player.new("computer", board2, ships)
 
     assert_equal board1, player1.board
+    assert_equal board2, player2.board
+  end
+
+  def test_if_player_has_lost
+    cruiser= Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    ships = [cruiser, submarine]
+
+    board1 = Board.new
+    board2 = Board.new
+
+    player1 = Player.new("human", board1, ships)
+    player2 = Player.new("computer", board2, ships)
+
+    assert_equal true, player1.has_lost?
+
   end
 end
