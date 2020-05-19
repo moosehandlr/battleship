1d2d66edb389c3c50f5449004bb9917ded1234b2
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-05-19 16:57:35 -0600

    create ship and cruiser for the player and computer.

--- lib/game.rb
+++ lib/game.rb
@@ -6,5 +6,14 @@ class Game
   def start
     player_board = Board.new
     computer_board = Board.new
+
+    player = Player.new(player_board)
+    computer = Player.new(computer_board)
+    # create comp and user cruiser and submarine
+    p_cruiser = Ship.new("Cruiser", 3)
+    c_cruiser = Ship.new("Cruiser", 3)
+    c_submarine = Ship.new("submarine", 2)
+    p_submarine = Ship.new("submarine", 2)
+    
   end
 end
