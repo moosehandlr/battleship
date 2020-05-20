73bdd2a118dfe5c67333b31df51d5280497a788e
Alex Desjardins <ardesjardins91@gmail.com>
2020-05-20 16:04:19 -0600

    Clean up runner file and add spacing.

--- runner.rb
+++ runner.rb
@@ -1,27 +1,25 @@
 require "./lib/game"
 space = "\n"
 
-puts "Welcome to BATTLESHIP"
+puts "Welcome to BATTLESHIP: Terminator edition!"
 puts space
 sleep(1)
-puts "Enter 'p' to start or 'q 'to quit"
-puts space
-sleep(0.5)
-puts "I have laid out my ships on the grid."
+puts "Terminator ships are now in position..."
 puts "You now need to lay out your two ships."
 puts "The Cruiser is three units long and the Submarine is two units long."
 
 
 loop do
+  puts "Enter s to start or q to quit"
   start_game_input = gets.chomp.upcase
   case start_game_input
-  when  "P"
+  when  "S"
     game = Game.new
     game.start
   when "Q"
-    puts "You quitted and let the terminators take over the world"
+    puts "The humans have lost and terminators have taken over the world"
     break
   else
-    puts "Please enter a valid input 'p or q'"
+    puts "Please enter a valid input 's or q'"
   end
 end
