171b741ab8298d377c35045d9162769e760dc189
Alex Desjardins <ardesjardins91@gmail.com>
2020-05-20 14:29:09 -0600

    Fix infinite loop when wrong start character entered.

--- runner.rb
+++ runner.rb
@@ -1,13 +1,19 @@
 require "./lib/game"
+space = "\n"
 
 puts "Welcome to BATTLESHIP"
+puts space
+sleep(1)
 puts "Enter 'p' to start or 'q 'to quit"
+puts space
+sleep(0.5)
 puts "I have laid out my ships on the grid."
 puts "You now need to lay out your two ships."
 puts "The Cruiser is three units long and the Submarine is two units long."
 
-start_game_input = gets.chomp.upcase
+
 loop do
+  start_game_input = gets.chomp.upcase
   case start_game_input
   when  "P"
     game = Game.new
